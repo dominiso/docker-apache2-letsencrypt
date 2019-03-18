172585fa2bf128a30859facf042a0463c8074354
dominiso <35030786+dominiso@users.noreply.github.com>
2019-03-18 10:07:50 +0100

    Update launcher.sh
    Remove the -f option for checking the hostname

--- launcher.sh
+++ launcher.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Checking certificates ( if /etc/letsencrypt/live/$(hostname -f)/privkey.pem exist )."
-if [[ ! -e /etc/letsencrypt/live/$(hostname -f)/privkey.pem ]]
+echo "Checking certificates ( if /etc/letsencrypt/live/$(hostname)/privkey.pem exist )."
+if [[ ! -e /etc/letsencrypt/live/$(hostname)/privkey.pem ]]
 then
   if [[ ! "x$LETS_ENCRYPT_DOMAINS" == "x" ]]; then
     DOMAIN_CMD="-d $(echo $LETS_ENCRYPT_DOMAINS | sed 's/,/ -d /')"
   fi
 
-  certbot-auto -n certonly --no-self-upgrade --agree-tos --standalone -t -m "$LETS_ENCRYPT_EMAIL" -d $(hostname -f) $DOMAIN_CMD
-  ln -s /etc/letsencrypt/live/$(hostname -f) /etc/letsencrypt/certs
+  certbot-auto -n certonly --no-self-upgrade --agree-tos --standalone -t -m "$LETS_ENCRYPT_EMAIL" $DOMAIN_CMD
+  ln -s /etc/letsencrypt/live/$(hostname) /etc/letsencrypt/certs
 else
   certbot-auto renew --no-self-upgrade
 fi
